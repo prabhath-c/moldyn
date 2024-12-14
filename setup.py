e3766cee964cf8a51528f4711f79729181b700e0
prabhath2197 <prabhath.21n@gmail.com>
2024-12-14 13:24:36 +0530

    setup edited

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_molecular_dynamics',
+    name='moldyn',
     version='0.1.0',
     author='Prabhath Chilakalapudi',
     description='A basic molecular dynamics simulation package',
