02bb80cc635cb24a7d37d00e29b50d11492de13f
prabhath2197 <prabhath.21n@gmail.com>
2024-12-14 21:30:22 +0530

    private functions added

--- system.py
+++ system.py
@@ -17,26 +17,26 @@ class System:
         self.accelerations = np.zeros_like(self.positions)
         self.atom_types = np.zeros(self.num_atoms)
 
-        self.initialize_positions()
-        self.initialize_atom_types()
-        self.initialize_velocities()
-        self.calculator = self.select_calculator()
-        self.integrator = self.select_integrator()
+        self.__initialize_positions()
+        self.__initialize_atom_types()
+        self.__initialize_velocities()
+        self.calculator = self.__select_calculator()
+        self.integrator = self.__select_integrator()
 
-    def initialize_positions(self):
+    def __initialize_positions(self):
         self.positions = np.random.uniform(0, self.box_size, (self.num_atoms, 3))
 
-    def initialize_atom_types(self):
+    def __initialize_atom_types(self):
         self.atom_types = np.random.choice(len(self.masses_list), self.num_atoms)
         self.masses = np.array([self.masses_list[type] for type in self.atom_types])
 
-    def initialize_velocities(self):
+    def __initialize_velocities(self):
         self.velocities = np.zeros((self.num_atoms, 3))
         for i, atom_type in enumerate(self.atom_types):
             mass = self.masses_list[atom_type]
             self.velocities[i] = np.random.normal(0, np.sqrt(self.temperature/mass), 3)
         
-    def select_calculator(self):
+    def __select_calculator(self):
         if self.potential == 'lj':
             return LennardJonesCalculator(self,
                                           epsilon=self.parameters["potential"][1],
@@ -45,7 +45,7 @@ class System:
         else:
             raise ValueError(f"Unsupported potential")
         
-    def select_integrator(self):
+    def __select_integrator(self):
         if self.integrator == 'velocity_verlet':
             return VelocityVerletIntegrator(self, self.parameters)
         else:
